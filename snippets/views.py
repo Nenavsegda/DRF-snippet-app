6b9804b9d13e9ee86222dad2955a1322f955f3da
Dmitry <dimachizhikov2@gmail.com>
2021-11-03 17:59:18 +0300

    add views.py, urls.py, refactor model

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 
-# Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
